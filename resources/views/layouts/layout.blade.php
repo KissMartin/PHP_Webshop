cc03a815e945da578f2a75992af427a402dae80d
Geher-Marcell <darkiex03@gmail.com>
2025-05-23 23:47:52 +0200

    feat: Add Font Awesome for enhanced icon support in search functionality

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HoneyHive</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-800 text-gray-200 overflow-x-hidden">
