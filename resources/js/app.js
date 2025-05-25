32d9eb873f9b2a5c004b2b9b04205bc1c7e55910
Geher-Marcell <darkiex03@gmail.com>
2025-05-25 15:45:13 +0200

    feat: Implement typewriter effect for search input; update profile menu styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,42 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+var writeSpeed = 2;
+var deleteSpeed = 3;
+var pauseTime = 1000;
+
+var typeList = ["Gaming accessories", "Everyday items", "Your favourite gadgets"];
+var startingWord = "Search for ";
+
+function loopTypewriter(elementId, texts, writeSpeed = 50, deleteSpeed = 50, pause = 1000, startingWord = "") {
+    const element = document.getElementById(elementId);
+    if (!element) return;
+    let index = 0;
+
+    function typeWriter(text, i = 0) {
+        if (i < text.length) {
+            element.placeholder = startingWord + text.substring(0, i + 1);
+            setTimeout(() => typeWriter(text, i + 1), 100/writeSpeed);
+        } else {
+            setTimeout(() => deleteWriter(text.length - 1), pause);
+        }
+    }
+
+    function deleteWriter(i) {
+        if (i >= 0) {
+            element.placeholder = startingWord + texts[index].substring(0, i);
+            setTimeout(() => deleteWriter(i - 1), 100/deleteSpeed);
+        } else {
+            index = (index + 1) % texts.length;
+            setTimeout(() => typeWriter(texts[index]), 100/writeSpeed);
+        }
+    }
+
+    element.placeholder = startingWord;
+    typeWriter(texts[index]);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loopTypewriter("typewriter", typeList, writeSpeed, deleteSpeed, pauseTime, startingWord);
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                     <h1 class="text-5xl font-bold">The highest rated webshop of 2025</h1>
                 </div>
                 <div class="bg-gray-800 h-[4rem] w-full flex rounded-4xl items-center justify-center">
-                    <input class="rounded-l-4xl h-full w-full text-2xl pl-5" type="text" placeholder="Search for your favourite items!">                    
+                    <input id="typewriter" class="rounded-l-4xl h-full w-full text-2xl pl-5" type="text" placeholder="Search for your favourite items!">                    
                     <a href="{{ route('products') }}" class="h-[80%] w-[10rem] bg-orange-700 rounded-3xl text-2xl mx-2 cursor-pointer flex items-center justify-center pb-0.5 hover:bg-orange-600"><i class="fa fa-search mr-2" aria-hidden="true"></i>Search</a>
                 </div>
             </div>
